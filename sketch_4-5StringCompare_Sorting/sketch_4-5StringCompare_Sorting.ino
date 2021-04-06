04ed7d22e0ac7b83d3b57ac15aafac88a0de75a9
park1997 <iambyunghyun97@gmail.com>
2021-04-07 01:49:33 +0900

    4-5StringCompare_Sorting

---
+++ sketch_4-5StringCompare_Sorting/sketch_4-5StringCompare_Sorting.ino
@@ -0,0 +1,29 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  String str[5] = {"bbb","aaa","eee","ddd","ccc"};
+  
+  //compareTo() : 문자열의 사전순 값을 비교하여 그에 해당되는 int 값을 리턴한다.
+  for(int i=0;i<4;i++){
+    for(int j =i+1 ; j<5;j++){
+      int compare = str[i].compareTo(str[j]);
+      if(compare>0){ //오름차순으로 정렬!
+        String temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+      }
+    }
+  }
+
+  for(int i=0;i<5;i++){
+    Serial.println(str[i]);
+  }
+
+
+
+
+}
