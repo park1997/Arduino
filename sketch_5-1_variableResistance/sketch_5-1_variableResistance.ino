51988ee2a125f9013ebaca8adeae1936ab6e6263
park1997 <iambyunghyun97@gmail.com>
2021-04-28 13:31:56 +0900

    Update sketch_5-1_variableResistance.ino

--- sketch_5-1_variableResistance/sketch_5-1_variableResistance.ino
+++ sketch_5-1_variableResistance/sketch_5-1_variableResistance.ino
@@ -3,10 +3,10 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   piMode(vResistor,INPUT);
-}기
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println(analogRead(vResistor));  //가변저항값을 읽어 출력
-  delay(1000);  // 1초 대
+  delay(1000);  // 1초 대기
 }
