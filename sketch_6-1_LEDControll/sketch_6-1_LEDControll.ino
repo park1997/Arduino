efb6aba7673bc365f516705cba910b326bc6df9d
park1997 <iambyunghyun97@gmail.com>
2021-05-02 14:48:28 +0900

    Update sketch_6-1_LEDControll.ino

--- sketch_6-1_LEDControll/sketch_6-1_LEDControll.ino
+++ sketch_6-1_LEDControll/sketch_6-1_LEDControll.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+int pin_button = 14;  // 버튼 연결핀
+int pin_LED1 = 13, pin_LED2 = 2;
+boolean LED_state1 = false;
+boolean LED_state2 = false;
 
+void setup() {
+  pinMode(pin_button,INPUT);
+  pinMode(pin_LED1,OUTPUT);
+  digitalWrite(pin_LED1,LED_state1);
+  pinMode(pin_LED2,OUTPUT);
+  digitalWrite(pin_LED2,LED_state2);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(pin_LED1,LED_state1);
+  delay(1000);
+  LED_state = !LED_state1;
 
+  if(digitalRead(pin_button))
+  
 }
