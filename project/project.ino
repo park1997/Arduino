ba87e0fe82390b525d9dbee38f6e9263eb57583f
park1997 <iambyunghyun97@gmail.com>
2021-06-13 18:33:20 +0900

    Update project.ino

--- project/project.ino
+++ project/project.ino
@@ -5,7 +5,7 @@
 #include <hd44780ioClass/hd44780_I2Cexp.h>  //lcd
 
 #include <core_build_options.h>
-#include <swRTC.h>
+#include <swRTC.h> // 시간모듈
 
 swRTC rtc;  //클래스 개체 선언
 
@@ -68,36 +68,34 @@ void setup(){
 void loop(){
     input = get_string(input);
     if(input == "reset"){
-      d_yellow = 50;
-      d_red = 10;
-    }else if( input.substring(0,1) == "y"){
+      d_yellow = 50;  // 노란불이 꺼지는 거리값 50cm로 재 초기화
+      d_red = 10; // 빨간불이 꺼지는 거리값 10cm로 재 초기화
+    }else if( input.substring(0,1) == "y"){ // 명령어의 첫번쨰 글자가 y일 경우
       // 명령어가 y로 시작하면서 바뀌는 수치값이 d_red보다 클 경우!(노란불점등 거리가 빨간불 점등 거리보다 짦을 순 없기떄문)
       if(input.substring(1).toInt() > d_red){
-        d_yellow = input.substring(1).toInt();
+        d_yellow = input.substring(1).toInt();  // 명령어의 숫자만큼의 거리값으로 변환함
         key1 = true;
-      }else if(key1 && key2){
+      }else if(key1 && key2){ // yellow의 거리값이 red보다 작아질경우(말이 안되는 상황)
         Serial1.print(d_red);
         Serial1.write("보다 크게 입력하셔야 합니다.\n");
         key1 = false;
       }
       
       
-    }else if(input.substring(0,1) =="r" ){
+    }else if(input.substring(0,1) =="r" ){  // 명령어의 첫번쨰 글자가 r일 경우
       // 명령어가 r로 시작하면서 바뀌는 수치값이 d_yellow보다 작을 경우만!( 빨간불 점등거리가 노란불 점등거리보다 클 수 없다.)
       if(input.substring(1).toInt() < d_yellow){
-        d_red = input.substring(1).toInt();
+        d_red = input.substring(1).toInt(); // 명령어의 숫자값으로 빨간불이 점등될 거리값 초기화
         key2 = true;
-      }else if (key1 && key2){
+      }else if (key1 && key2){  // red의 거리값이 yellow의 거리값보다 커질경우 ( 말이 안되는 경우)
         Serial1.print(d_yellow);
         Serial1.write("보다 작게 입력하셔야 합니다.\n");
         key2 = false;
       }
-      
-      
     }
   
   
-    int cur = digitalRead(button);
+    int cur = digitalRead(button);  // 버튼의 상태 검사
     // 디바운싱 해결
     if(cur != before){
       delay(5);
@@ -147,9 +145,8 @@ void showDistance(){
       pinMode(echoPin, INPUT);
     }
     if( distance <= 3){
-      time();
-      Serial.write("   충돌하였습니다\n");
-//      time();  // 현재 시간 출력
+      time(); // 현재의 시간 출력
+      Serial.write("   충돌하였습니다\n"); // 충돌메세지 전달
       Serial1.write("   충돌하였습니다\n");
     }
     
@@ -168,24 +165,24 @@ void showLight(){
    * 400 평소 형광등
    * 800 어두움
    */
-  int cds = analogRead(A1);
+  int cds = analogRead(A1); // 현재의 조도값을 읽어옴
   int bright = map(cds, 0, 1023, 255, 0); // 어두워지면 LED도 어두워지게 역매핑
 //  Serial.println(cds);
 
   if (distance < d_red){
      // 거리가 d_red 미만일시 빨간불 표시하는데 현재 밝기에 고려하여 빛 발광
-    analogWrite(red_led, bright);
+    analogWrite(red_led, bright); // 현재 조도값에 반비례하여 밝기 조정
     digitalWrite(green_led, LOW);
     digitalWrite(yellow_led,LOW);
   }else if(distance < d_yellow){
     // // 거리가 d_red 초과 d_yellow 이상일시 노란불 표시, 현재 밝기에 고려하여 빛 발광
-    analogWrite(yellow_led, bright);
+    analogWrite(yellow_led, bright);// 현재 조도값에 반비례하여 밝기 조정
     digitalWrite(green_led, LOW);
     digitalWrite(red_led, LOW);    
   }else{
     // 거리가 d_yellow 이상일시 초록불 표시, 현재 밝기에 고려하여 빛 발광
     digitalWrite(red_led, LOW);
-    analogWrite(green_led, bright);
+    analogWrite(green_led, bright);// 현재 조도값에 반비례하여 밝기 조정
     digitalWrite(yellow_led, LOW);
   }
   delay(100);
@@ -204,20 +201,18 @@ String get_string(String input){
   else return inputString;             // 데이터를 입력받았다면 해당 문자열을 출력
 }
 
+// 현재 시간 출력함수
 void time(){
-  Serial1.print(rtc.getYear(), DEC);
+  Serial1.print(rtc.getYear(), DEC);  // 연도
   Serial1.print("/");
-  Serial1.print(rtc.getMonth(), DEC);
+  Serial1.print(rtc.getMonth(), DEC); // 월
   Serial1.print("/");
-  Serial1.print(rtc.getDay(), DEC);
+  Serial1.print(rtc.getDay(), DEC); // 일
   Serial1.print("\t"); 
   
-  Serial1.print(rtc.getHours(), DEC);
+  Serial1.print(rtc.getHours(), DEC); // 시
   Serial1.print(":");
-  Serial1.print(rtc.getMinutes(), DEC);
+  Serial1.print(rtc.getMinutes(), DEC); // 분
   Serial1.print(":");
-  Serial1.print(rtc.getSeconds(), DEC);
-  
-
-  
+  Serial1.print(rtc.getSeconds(), DEC); // 초
 }
